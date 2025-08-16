c4bf81dbeda00f7b9130285cd655aae79a738a29
Razanhaweizy <Razanrh05@gmail.com>
2025-08-16 19:20:44 +0300

    print error msg

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  input[strcspn(input, "\n")] = 0;
   printf("%s: command not found\n", input);
   return 0;
 }
